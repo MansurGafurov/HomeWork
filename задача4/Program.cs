2fedb469dff8fe25a8377dfc43a48143051d07f1
Mansur Gafurov <Gafurovmansur1@yandex.ru>
2022-11-21 22:35:24 +0500

    add task 4

---
+++ задача4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите три числа");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+int C = Convert.ToInt32(Console.ReadLine());
+
+if(A>B && A>C)
+{
+    Console.WriteLine( "Max =" +A );
+}
+else if(B>A && B>C)
+{
+    Console.WriteLine( "Max =" +B );
+}
+else 
+{
+    Console.WriteLine ("Max =" +C);
+}
+
+
