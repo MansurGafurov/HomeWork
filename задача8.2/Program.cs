4923d80c35eea2c4439eefdedb77012f41dcd146
Mansur Gafurov <Gafurovmansur1@yandex.ru>
2022-11-23 16:00:40 +0500

    add task8.2

---
+++ задача8.2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите целое число");
+int N = Convert.ToInt32(Console.ReadLine());
+int I = 1;
+while(I <= N)
+{
+    if((I % 2) == 0)
+    {
+       Console.Write(I + ", ");
+    }
+    I ++;
+}
+
