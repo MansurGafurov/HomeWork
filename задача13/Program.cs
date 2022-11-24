685c9bc3be4748e662976b3f85dc2f3a5e221c9e
Mansur Gafurov <Gafurovmansur1@yandex.ru>
2022-11-24 19:38:18 +0500

    add task 13 - NO ENDgit add . git add . !

---
+++ задача13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine("Введите любое цело число");
+int Number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Number);
+if(Number <= 99)
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
+else
+{
+    int Number3 = Number % 10;
+    Console.WriteLine(Number3);
+}
