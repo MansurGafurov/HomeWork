bdd129d5fbd4d1120679061685b6fb15a561ab76
Mansur Gafurov <Gafurovmansur1@yandex.ru>
2022-12-16 01:06:31 +0500

    задача 60

---
+++ задача56/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу,
+//  которая будет находить строку с наименьшей суммой элементов.
+// Программа считает сумму элементов в каждой строке 
+// и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+Console.WriteLine("Введите резмер массива");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+Console.WriteLine();
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArray(array);
+Console.WriteLine();
+
+List<int> SumStrings = new List<int>();
+
+// int ReturnList(List<int> SumStrings)
+// {
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    int sum = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        sum += array[i, j];
+    }
+    SumStrings.Add(sum);
+}
+// return List<int> SumStrings; 
+//     return SumStrings.Add(sum);
+// }
+
+// ReturnList(SumStrings);
+Console.WriteLine();
+
+int ReturnIndex(List<int> SumStrings)
+{
+    int MaxIDX = 0;
+    int Max = 0;
+
+    for (int i = 0; i < SumStrings.Count; i++)
+    {
+        if (SumStrings[i] > Max)
+        {
+            Max = SumStrings[i];
+            MaxIDX = i;
+        }
+    }
+    return MaxIDX;
+}
+Console.WriteLine();
+Console.WriteLine($" Строка с наибольшей суммой элементов: {ReturnIndex(SumStrings) + 1}");
+
+
---
+++ задача60/Program.cs
@@ -0,0 +1,27 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+Console.WriteLine("Задайте размер трехмерного массива");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int r = Convert.ToInt32(Console.ReadLine());
+int[,,] Array = new int[m, n, r];
+Console.WriteLine();
+
+int count = 10; 
+for (int i = 0; i < Array.GetLength(0); i++)
+{
+    for (int j = 0; j < Array.GetLength(1); j++)
+    {
+        for (int k = 0; k < Array.GetLength(2); k++)
+        {
+            Array[i, j, k] = count;
+            Console.WriteLine($"In Array [{i}, {j}, {k}] the number is stored {Array[i, j, k]} ");
+            count++;
+        }
+    }
+}
