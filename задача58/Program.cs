bafef1a088e45365ae2b00c6f1a6f763fd0ec81e
Mansur Gafurov <Gafurovmansur1@yandex.ru>
2022-12-17 12:00:35 +0500

    Задача 58

---
+++ задача58/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+int[,] ArrA = { { 1, 2, 3 }, { 4, 5, 6 } };
+int[,] ArrB = { { 1, 2, 3 }, { 4, 5, 6 } };
+int[,] ArrC = new int[2, 3];
+
+if (ArrA.GetLength(0) == ArrB.GetLength(0) && ArrA.GetLength(1) == ArrB.GetLength(1))
+{
+    for (int i = 0; i < ArrC.GetLength(0); i++)
+    {
+        for (int j = 0; j < ArrC.GetLength(1); j++)
+        {
+            ArrC[i, j] = ArrA[i, j] * ArrB[i, j];
+            Console.Write($" {ArrC[i, j]} \t ");
+        }
+        Console.WriteLine();
+    }
+}
+else Console.WriteLine($"Произведение матриц не возможно!");
+
+
+
+
+
+
+
+
+
